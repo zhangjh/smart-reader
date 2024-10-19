3d129823433b6c6d8e08337474d6fa6d07afd924
zhangjh <njhxzhangjihong@126.com>
2024-10-19 18:17:58 +0800

    fix

--- app/reader/page.tsx
+++ app/reader/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import {useState } from 'react';
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import NavBar from '@/components/NavBar';
