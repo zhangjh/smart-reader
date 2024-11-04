0ebca1b211542809bf2d45efdab119af8e4c34f9
zhangjh <njhxzhangjihong@126.com>
2024-11-04 14:44:59 +0800

    update

--- app/translation/page.tsx
+++ app/translation/page.tsx
@@ -1,13 +1,13 @@
 'use client'
 
-import React, { useEffect, useState } from 'react';
+// import React, { useEffect, useState } from 'react';
 import Sidebar from '@/components/SideBar';
-import { Card } from "@/components/ui/card";
-import { Progress } from "@/components/ui/progress";
-import { toast } from 'react-toastify';
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
-import util from '@/utils/util';
+// import { Card } from "@/components/ui/card";
+// import { Progress } from "@/components/ui/progress";
+// import { toast } from 'react-toastify';
+// import { Input } from '@/components/ui/input';
+// import { Button } from '@/components/ui/button';
+// import util from '@/utils/util';
 
 const Translation = () => {
     return (
@@ -15,7 +15,7 @@ const Translation = () => {
             <div className="p-4 md:p-6">
                 <div className="max-w-4xl mx-auto">
                     <div className="flex flex-col md:flex-row justify-between items-center mb-6">
-                        
+                        <span>开发中，敬请期待...</span>                        
                     </div>
 
                 </div>
