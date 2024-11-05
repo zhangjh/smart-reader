cf10b6569e0d81bb86499d96a34afb64795b713e
zhangjh <njhxzhangjihong@126.com>
2024-11-06 00:39:00 +0800

    修改底部信息

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,91 +1,113 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { IoLogoWechat } from "react-icons/io5";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
+  const [showWx, setShowWx] = useState(false);
+
   return (
-    <footer className="bg-gray-100">
-      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
-        <div className="xl:grid xl:grid-cols-3 xl:gap-8">
-          <div className="space-y-8 xl:col-span-1">
-            <span className="text-2xl font-bold text-blue-600">智阅</span>
-            <p className="text-gray-500 text-base">
-              提升您的阅读体验，让学习更加高效。
+    <>
+      <footer className="bg-gray-100">
+        <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
+          <div className="xl:grid xl:grid-cols-3 xl:gap-8">
+            <div className="space-y-8 xl:col-span-1">
+              <span className="text-2xl font-bold text-blue-600">智阅</span>
+              <p className="text-gray-500 text-base">
+                提升您的阅读体验，让学习更加高效。
+              </p>
+              <div className="flex space-x-6">
+                <a className="text-gray-400 hover:text-gray-500" onClick={() => setShowWx(true)}>
+                  <span className="sr-only">微信</span>
+                  <IoLogoWechat />
+                </a>
+                <a target='_blank' href="https://x.com/Dante_Chaser" className="text-gray-400 hover:text-gray-500">
+                  <span className="sr-only">推特</span>
+                  <FaXTwitter />
+                </a>
+                <a target='_blank' href="https://github.com/zhangjh" className="text-gray-400 hover:text-gray-500">
+                  <span className="sr-only">GitHub</span>
+                  <FaGithub />
+                </a>
+              </div>
+            </div>
+            <div className="mt-12 xl:mt-0 xl:col-span-2">
+              <div className="grid grid-cols-2 gap-8">
+                <div>
+                  <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase">
+                    解决方案
+                  </h3>
+                  <ul className="mt-4 space-y-4">
+                    <li>
+                      <a href="#" className="text-base text-gray-500 hover:text-gray-900">
+                        电子书阅读
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" className="text-base text-gray-500 hover:text-gray-900">
+                        AI总结
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" className="text-base text-gray-500 hover:text-gray-900">
+                        个人知识库
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" className="text-base text-gray-500 hover:text-gray-900">
+                        智能问答
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                <div>
+                  <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase">
+                    支持
+                  </h3>
+                  <ul className="mt-4 space-y-4">
+                    <li>
+                      <a href="#" className="text-base text-gray-500 hover:text-gray-900">
+                        使用指南
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" className="text-base text-gray-500 hover:text-gray-900">
+                        常见问题
+                      </a>
+                    </li>
+                    <li>
+                      <a href="mailto:zhangjh_initial@126.com" className="text-base text-gray-500 hover:text-gray-900">
+                        联系我们
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mt-12 border-t border-gray-200 pt-8">
+            <p className="text-base text-gray-400 xl:text-center">
+              &copy; 2024 智阅@太初软件. 保留所有权利.
             </p>
-            <div className="flex space-x-6">
-              <a href="#" className="text-gray-400 hover:text-gray-500">
-                <span className="sr-only">微博</span>
-                <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                  <path fillRule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clipRule="evenodd" />
-                </svg>
-              </a>
-              <a href="#" className="text-gray-400 hover:text-gray-500">
-                <span className="sr-only">微信</span>
-                <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                  <path fillRule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clipRule="evenodd" />
-                </svg>
-              </a>
-            </div>
-          </div>
-          <div className="mt-12 xl:mt-0 xl:col-span-2">
-            <div className="grid grid-cols-2 gap-8">
-              <div>
-                <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase">
-                  解决方案
-                </h3>
-                <ul className="mt-4 space-y-4">
-                  <li>
-                    <a href="#" className="text-base text-gray-500 hover:text-gray-900">
-                      电子书阅读
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#" className="text-base text-gray-500 hover:text-gray-900">
-                      AI总结
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#" className="text-base text-gray-500 hover:text-gray-900">
-                      个人知识库
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#" className="text-base text-gray-500 hover:text-gray-900">
-                      智能问答
-                    </a>
-                  </li>
-                </ul>
-              </div>
-              <div>
-                <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase">
-                  支持
-                </h3>
-                <ul className="mt-4 space-y-4">
-                  <li>
-                    <a href="#" className="text-base text-gray-500 hover:text-gray-900">
-                      使用指南
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#" className="text-base text-gray-500 hover:text-gray-900">
-                      常见问题
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#" className="text-base text-gray-500 hover:text-gray-900">
-                      联系我们
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
           </div>
         </div>
-        <div className="mt-12 border-t border-gray-200 pt-8">
-          <p className="text-base text-gray-400 xl:text-center">
-            &copy; 2024 智阅@太初软件. 保留所有权利.
-          </p>
+      </footer>
+
+      { showWx && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+          <div className="relative">
+            <img 
+              src="/imgs/wechat.jpg" 
+              alt="微信二维码" 
+              width={200}
+              className="object-contain"
+              onClick={() =>setShowWx(false)}
+            />
+          </div>
         </div>
-      </div>
-    </footer>
+      )}
+    </>
+   
   );
 };
 
