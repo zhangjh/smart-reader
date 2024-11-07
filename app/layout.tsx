6e6210a1fcbddc9016edb18fca54930e5dae9f49
zhangjh <njhxzhangjihong@126.com>
2024-11-07 17:26:53 +0800

    增加百度验证meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,9 @@ export const metadata: Metadata = {
   title: "智阅 - 您的智能阅读伙伴",
   keywords: "智阅,AI伴读,电子书下载,epub阅读器,AI总结,AI翻译,文档问答,书籍内容总结",
   description: "智阅：结合电子书阅读、AI总结、个人知识库和智能问答，提升您的阅读和学习体验。",
+  other: {
+    "baidu-site-verification": "codeva-fauiyMT50E",
+  }
 };
 
 export default function RootLayout({
